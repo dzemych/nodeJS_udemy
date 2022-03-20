e0c113216215bac5f1f4b7b2821d08936f724474
Reefleexx <dzemichivan@gmail.com>
2022-03-20 10:37:53 +0200

    doing pug

--- src/dev-data/data/import-dev-data.js
+++ src/dev-data/data/import-dev-data.js
@@ -21,7 +21,7 @@ mongoose
 
 // READ JSON FILE
 const tours = JSON.parse(
-   fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+   fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
 );
 const users = JSON.parse(
    fs.readFileSync(`${__dirname}/users.json`, 'utf-8')
@@ -34,8 +34,8 @@ const reviews = JSON.parse(
 const importData = async () => {
   try {
     await Tour.create(tours);
-    await User.create(users);
-    await Review.create(reviews);
+    // await User.create(users);
+    // await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -47,8 +47,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
-    await User.deleteMany();
-    await Review.deleteMany();
+    // await User.deleteMany();
+    // await Review.deleteMany();
     console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
